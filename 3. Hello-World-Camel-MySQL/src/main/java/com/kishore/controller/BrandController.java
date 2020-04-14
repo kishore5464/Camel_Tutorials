b389bb6dafcedea3de21089ad845b63e426cd351
Kishore Kumar <kishorekumar5464@gmail.com>
2020-04-14 12:40:38 +0530

    Insert into MySQL Db

--- 3. Hello-World-Camel-MySQL/src/main/java/com/kishore/controller/BrandController.java
+++ 3. Hello-World-Camel-MySQL/src/main/java/com/kishore/controller/BrandController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.apache.camel.ProducerTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kishore.models.CarBrands;
@@ -20,6 +22,12 @@ public class BrandController {
 		@SuppressWarnings("unchecked")
 		List<CarBrands> brands = producerTemplate.requestBody("direct:select", null, List.class);
 		return brands;
+	}
+
+	@PostMapping(value = "/addBrand")
+	public boolean insertBrand(@RequestBody CarBrands brands) {
+		producerTemplate.requestBody("direct:insert", brands, List.class);
+		return true;
 	}
 
 }
